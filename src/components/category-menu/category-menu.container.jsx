772c7d1b10b4975c9f12248ab94f3d6986f80716
CyPhEr420 <vimalvk9900@gmail.com>
2023-08-03 17:10:32 +0530

    initial commit

---
+++ src/components/category-menu/category-menu.container.jsx
@@ -0,0 +1,18 @@
+import './category-menu.styles.scss'
+import CategoryItem from '../category-item/category-item.container';
+
+const CategoryMenu = ({ categories }) => {
+
+
+    return (
+        <div className="categories-container">
+            {
+                categories.map((category) => (
+                    <CategoryItem category={category} key={category.id} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default CategoryMenu
